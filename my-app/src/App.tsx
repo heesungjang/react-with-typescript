c3ec82266f5f8858a5c894ec3ed05403452d7dba
Heesung jang <heesungj7@gmail.com>
2021-06-24 00:13:43 +0900

    20210624 - react-ts-props
    declare types for class components and functional components

---
+++ my-app/src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Number from "./Number";
+
+interface IState {
+    counter: number;
+}
+
+class App extends React.Component<{}, IState> {
+    state = {
+        counter: 0,
+    };
+
+    render() {
+        const { counter } = this.state;
+        return (
+            <div>
+                <Number count={counter} />
+                <button onClick={this.add}>Add</button>
+            </div>
+        );
+    }
+    add = () => {
+        this.setState((prev) => {
+            return {
+                counter: prev.counter + 1,
+            };
+        });
+    };
+}
+
+export default App;
